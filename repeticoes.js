e3d52e8cf2eb86685a38f759f83c3b6dfb64ab1d
FIC <fic@info.local>
2025-04-07 21:41:19 -0300

    finalizacao

---
+++ repeticoes.js
@@ -0,0 +1,39 @@
+function exemplofor() {
+    //FOR
+    //1o = declarar contador.
+    //2o = Condição (enquanto).
+    //3o = Incremento.
+
+    for (let contador = 1; contador <= 5; contador++) {
+
+        alert(contador);
+    }
+
+}
+
+function exemplowhile() {
+
+    let contador = 1;
+
+    while (contador <= 5) {
+
+        console.log(contador);
+        contador++
+    }
+
+}
+
+function exenploDowhile() {
+
+    let(contador = 1);
+
+    do {
+
+        alert(contador);
+        contador++
+
+    } while (contador <= 5);
+
+
+}
+exemploDowhile() 
